0864c395f3a24f9b54adc4ae7408e0b932b135e4
Abdllah_Atef <abdllahatef7@gmail.com>
2026-02-22 21:22:40 +0200

    SmartUniversityAPP

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SmartUniversityApp());
+}
+
+class SmartUniversityApp extends StatelessWidget {
+  const SmartUniversityApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
